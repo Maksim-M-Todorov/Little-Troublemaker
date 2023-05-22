1cc8b2eac4638c2d0ebc40a32319637ea439a04f
Maksim-M-Todorov <maksi02.mt@gmail.com>
2023-05-22 18:22:08 +0200

    Update Menu

---
+++ My project (1)/Assets/Scenes/Upgrade Menu/UpgradePassive.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradePassive : MonoBehaviour
+{
+    public Inventory inv;
+    public MoneyCounter mC;
+
+    public void SolarPanels()
+    {
+        inv.SolarPanels = true;
+        inv.currentMoney -= 5000;
+    }
+
+    public void EnergyTiles()
+    {
+        inv.EnergyTiles = true;
+        inv.currentMoney -= 10000;
+    }
+
+    public void ImprovedIsolation()
+    {
+        inv.ImprovedIsolation = true;
+        inv.currentMoney -= 5000;
+    }
+
+    public void LEDBulbs()
+    {
+        inv.LEDBulbs = true;
+        inv.currentMoney -= 1000;
+    }
+
+    private void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}
+
