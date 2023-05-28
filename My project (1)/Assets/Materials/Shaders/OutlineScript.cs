75343ee86b9de45a1681aeab57f7f4edffef4980
Maksim-M-Todorov <maksi02.mt@gmail.com>
2023-05-28 13:17:39 +0200

    PostProcess Shader for Outline added

---
+++ My project (1)/Assets/Materials/Shaders/OutlineScript.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class OutlineScript : MonoBehaviour
+{
+    [Serializable]
+    [UnityEngine.Rendering.PostProcessing.PostProcess(typeof(PostProcessOutlineRenderer), PostProcessEvent.AfterStack, "Outline")]
+    public sealed class PostProcessOutline : PostProcessEffectSettings
+    {
+        public FloatParameter thickness = new FloatParameter { value = 1f };
+        public FloatParameter depthMin = new FloatParameter { value = 0f };
+        public FloatParameter depthMax = new FloatParameter { value = 1f };
+    }
+
+    public class PostProcessOutlineRenderer : PostProcessEffectRenderer<PostProcessOutline>
+    {
+        public override void Render(PostProcessRenderContext context)
+        {
+            PropertySheet sheet = context.propertySheets.Get(Shader.Find("Hidden/Outline"));
+            sheet.properties.SetFloat("_Thickness", settings.thickness);
+            sheet.properties.SetFloat("_MinDepth", settings.depthMin);
+            sheet.properties.SetFloat("_MaxDepth", settings.depthMax);
+
+            context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
+        }
+    }
+}
